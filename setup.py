08b425b4334efb46a86010fb51c5c2abb888bf2a
Karl Voit <git@Karl-Voit.at>
2019-12-30 19:21:36 +0100

    Version 2019.12.30.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name="appendorgheading",
-    version="2019.12.30.1",
+    version="2019.12.30.2",
     description="Append a new Org mode heading to an existing Org mode file",
     author="Karl Voit",
     author_email="tools@Karl-Voit.at",
