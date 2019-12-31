de91adabcd94560f0da14f4b461757aafcca58ef
Karl Voit <git@Karl-Voit.at>
2019-12-31 15:20:23 +0100

    test_minimal_output

---
+++ tests/test_minimal_output/runtest.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+output="output1.org"
+
+# initialize the output file with a nice heading:
+echo "* This is the main heading   :tag1:" > "${output}"
+
+../../appendorgheading/__init__.py
+
+ # end
+
+
